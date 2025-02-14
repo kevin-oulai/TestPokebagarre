475a2fd8abd345c35b45191ad32f4bc0abadbe1b
Kevin Oulai <koulai001@univ-pau.fr>
2025-02-14 12:22:45 +0100

    Methodes remplies et executées

--- src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -12,7 +12,10 @@
 
 package com.montaury.pokebagarre.metier;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import com.montaury.pokebagarre.fixtures.ConstructeurDePokemon;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -21,45 +24,55 @@ class PokemonTest {
     @Test
      void pokemon1_devrait_gagner_avec_attaque_superieure(){
          // GIVEN
-
+        Pokemon pokemon1 = new ConstructeurDePokemon().avecAttaque(10).avecDefense(10).construire();
+        Pokemon pokemon2 = new ConstructeurDePokemon().avecAttaque(5).avecDefense(10).construire();
          // WHEN
-
+        boolean resultat = pokemon1.estVainqueurContre(pokemon2);
          // THEN
+        assertThat(resultat).isTrue();
      }
 
     @Test
     void pokemon2_devrait_gagner_avec_attaque_superieure(){
         // GIVEN
-
+        Pokemon pokemon1 = new ConstructeurDePokemon().avecAttaque(5).avecDefense(10).construire();
+        Pokemon pokemon2 = new ConstructeurDePokemon().avecAttaque(10).avecDefense(10).construire();
         // WHEN
-
+        boolean resultat = pokemon1.estVainqueurContre(pokemon2);
         // THEN
+        assertThat(resultat).isFalse();
     }
 
     @Test
     void pokemon1_devrait_gagner_avec_defense_superieure(){
         // GIVEN
-
+        Pokemon pokemon1 = new ConstructeurDePokemon().avecAttaque(10).avecDefense(10).construire();
+        Pokemon pokemon2 = new ConstructeurDePokemon().avecAttaque(10).avecDefense(5).construire();
         // WHEN
-
+        boolean resultat = pokemon1.estVainqueurContre(pokemon2);
         // THEN
+        assertThat(resultat).isTrue();
     }
 
     @Test
     void pokemon2_devrait_gagner_avec_defense_superieure(){
         // GIVEN
-
+        Pokemon pokemon1 = new ConstructeurDePokemon().avecAttaque(10).avecDefense(5).construire();
+        Pokemon pokemon2 = new ConstructeurDePokemon().avecAttaque(10).avecDefense(10).construire();
         // WHEN
-
+        boolean resultat = pokemon1.estVainqueurContre(pokemon2);
         // THEN
+        assertThat(resultat).isFalse();
     }
 
     @Test
-    void pokemon1_devrait_gagner_en_cas_degalite(){
+    void pokemon1_devrait_gagner_en_cas_de_statistiques_egales(){
         // GIVEN
-
+        Pokemon pokemon1 = new ConstructeurDePokemon().avecAttaque(10).avecDefense(10).construire();
+        Pokemon pokemon2 = new ConstructeurDePokemon().avecAttaque(10).avecDefense(10).construire();
         // WHEN
-
+        boolean resultat = pokemon1.estVainqueurContre(pokemon2);
         // THEN
+        assertThat(resultat).isTrue();
     }
 }
